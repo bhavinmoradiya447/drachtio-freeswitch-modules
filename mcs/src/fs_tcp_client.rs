c090a10f1b6a275ce1e09fa7fc15e65fae4f78fc
bmoradiy <bmoradiy@cisco.com>
2024-03-13 21:08:44 +0530

    Refactor

--- mcs/src/fs_tcp_client.rs
+++ mcs/src/fs_tcp_client.rs
@@ -12,6 +12,7 @@ use tokio::net::{TcpStream, ToSocketAddrs};
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use tokio::time::sleep;
 use crate::CONFIG;
+use tracing::{error, info};
 
 #[derive(Debug)]
 struct CommandClient(TcpStream);
@@ -28,55 +29,55 @@ impl<A> UnderlyingIo<A> for CommandClient
                 Ok(mut stream) => {
                     let mut buf = [0; 128];
                     let auth_command = format!("auth {}\n\n", CONFIG.fs_esl_client.auth.clone());
-                    let size = stream.read(&mut buf).await.expect("TODO: panic message");
+                    let size = stream.read(&mut buf).await.unwrap();
                     let is_login_success = match String::from_utf8(buf[0..size].to_owned()) {
                         Ok(str) => {
                             match str.as_str().contains("Content-Type: auth/request") {
                                 true => {
-                                    stream.write(auth_command.as_bytes()).await.expect("Failed to send command");
-                                    stream.read(&mut buf).await.expect("Failed to read Command response");
+                                    stream.write(auth_command.as_bytes()).await.unwrap();
+                                    stream.read(&mut buf).await.unwrap();
                                     match String::from_utf8(Vec::from(buf)) {
                                         Ok(str) => {
                                             match str.as_str().contains("Reply-Text: +OK accepted") {
                                                 true => {
-                                                    println!("Login Success");
+                                                    info!("Successfully login to ESL Socket");
                                                     true
                                                 }
                                                 _ => {
-                                                    println!("Login Failed, {}", str);
-                                                    stream.shutdown().await.expect("Failed to close connection");
+                                                    info!("Failed to login to ESL Socket {}", str);
+                                                    stream.shutdown().await.unwrap();
                                                     false
                                                 }
                                             }
                                         }
                                         Err(e) => {
-                                            println!("got error while login {}", e);
-                                            stream.shutdown().await.expect("Failed to close connection");
+                                            error!("Got an error while login to ESL Socket {}", e);
+                                            stream.shutdown().await.unwrap();
                                             false
                                         }
                                     }
                                 }
                                 false => {
-                                    println!("Failed to get auth request {}", str);
-                                    stream.shutdown().await.expect("Failed to close connection");
+                                    error!("Failed to get auth request on ESL Socket {}", str);
+                                    stream.shutdown().await.unwrap();
                                     false
                                 }
                             }
                         }
                         Err(e) => {
-                            println!("failed with {}", e);
+                            error!("Failed to get auth request on ESL Socket {}", e);
                             false
                         }
                     };
                     if is_login_success {
                         Ok(CommandClient(stream))
                     } else {
-                        Err(Error::new(io::ErrorKind::Other, "Connection Failed"))
+                        Err(Error::new(io::ErrorKind::ConnectionRefused, "Connection Failed"))
                     }
                 }
                 Err(e) => {
-                    println!("Failed to connect to Socket, {}", e);
-                    Err(Error::new(io::ErrorKind::Other, "Connection Failed"))
+                    error!("Failed to connect to ESL Socket {}", e);
+                    Err(Error::new(io::ErrorKind::ConnectionRefused, "Connection Failed"))
                 }
             }
         })
@@ -115,24 +116,24 @@ async fn process_command(mut command_client: StubbornIo<CommandClient, String>, 
     while let Some(msg) = rx.recv().await {
         match command_client.write(format!("{}\n\n", msg).as_bytes()).await {
             Ok(size) => {
-                println!("Sent command with size {}", size);
+                info!("Sent command with size {}", size);
                 let mut buf = [0; 100];
-                let size = command_client.read(&mut buf).await.expect("Failed to get Command Response");
-                println!("Got response {:?}", String::from_utf8(buf[0..size].to_owned()).unwrap())
+                let size = command_client.read(&mut buf).await.unwrap();
+                info!("Got response {:?}", String::from_utf8(buf[0..size].to_owned()).unwrap())
             }
             Err(e) => {
-                println!("Failed to send event to FS {}", e);
+                error!("Failed to send event to FS {}", e);
                 sleep(Duration::from_secs(1)).await;
                 tx.send(msg).unwrap();
             }
         }
     }
 }
-//}
+
 
 type FsCommandClient<A> = StubbornIo<CommandClient, A>;
 
-pub async fn start_fs_esl_client(event_receiver: UnboundedReceiver<String>, event_sender: UnboundedSender<String>, host_name: String) ->  Result<(), Box<dyn std::error::Error>>{
+pub async fn start_fs_esl_client(event_receiver: UnboundedReceiver<String>, event_sender: UnboundedSender<String>, host_name: String) -> Result<(), Box<dyn std::error::Error>> {
     let options = ReconnectOptions::new().with_exit_if_first_connect_fails(false).with_retries_generator(|| {
         iter::repeat(Duration::from_secs(1))
     });
