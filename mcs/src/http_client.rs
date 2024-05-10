756fa3e52c962827d1176abd457a58654ed073b5
bmoradiy <bmoradiy@cisco.com>
2024-05-10 11:13:23 +0530

    added log

--- mcs/src/http_client.rs
+++ mcs/src/http_client.rs
@@ -14,7 +14,6 @@ impl HttpClient {
             client: Client::builder()
                 .connect_timeout(Duration::from_millis(5000))
                 .http2_keep_alive_while_idle(true)
-                .http2_prior_knowledge()
                 .http2_keep_alive_timeout(Duration::from_millis(5000))
                 .http2_keep_alive_interval(Some(Duration::from_millis(10000)))
                 .build().unwrap()
@@ -22,11 +21,11 @@ impl HttpClient {
     }
 
     pub async fn is_call_leg_exist(&self, uuid: String) -> bool {
-        info!("UserName {}, password {}", CONFIG.fs_http_client.user_name.clone(), CONFIG.fs_http_client.password.clone());
         let request_url = format!("http://127.0.0.1:7080/xmlapi/uuid_exists?{}", uuid);
         match self.client.get(request_url)
+            .basic_auth(CONFIG.fs_http_client.user_name.clone(), Some(CONFIG.fs_http_client.password.clone()))
             .timeout(Duration::from_secs(3))
-            .basic_auth(CONFIG.fs_http_client.user_name.clone(), Some(CONFIG.fs_http_client.password.clone())).send().await {
+            .send().await {
             Ok(res) => {
                 match res.status().is_success() {
                     true => {
