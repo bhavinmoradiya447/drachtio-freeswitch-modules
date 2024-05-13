c941f53f15049a3a25c601795b62cdbb5d9f8537
bmoradiy <bmoradiy@cisco.com>
2024-05-13 14:04:10 +0530

    serial test

--- mcs-it/tests/integration_test.rs
+++ mcs-it/tests/integration_test.rs
@@ -210,7 +210,7 @@ async fn test() {
 
     let map = GLOBAL_MAP.lock().unwrap();
     info!("Event value map {:?}", map);
-    assert_eq!(&8, map.get("start").unwrap());
+    assert_eq!(&10, map.get("start").unwrap());
     assert_eq!(&1, map.get("stop").unwrap());
     assert_eq!(&3, map.get("failed").unwrap());
     assert_eq!(&6, map.get("event").unwrap());
